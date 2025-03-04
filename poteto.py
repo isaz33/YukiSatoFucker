bc66dc0dc3d06875fc790a329c7a8848330855c4
isaz33 <subsy1233@gmail.com>
2025-03-05 01:52:49 +0900

    Update poteto.py

--- poteto.py
+++ poteto.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 from keep_alive import keep_alive
 from flask import Flask, request
-from datetime import datetime
+from datetime import timedelta
 
 intents = discord.Intents.default()
 intents.message_content = True
