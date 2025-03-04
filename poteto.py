c41c430869fade8650ee2727e57be023dd76dd3e
isaz33 <subsy1233@gmail.com>
2025-03-05 02:12:45 +0900

    Update poteto.py

--- poteto.py
+++ poteto.py
@@ -36,7 +36,7 @@ async def on_message(message):
             # timeout_duration = discord.utils.utcnow() + discord.timedelta(minutes=0.1)
             # await target_user.edit(timeout=timeout_duration)
             await target_user.timeout(timedelta(minutes=1), reason="ホモのためタイムアウト")
-            await message.channel.send(f"ホモ({target_user.mention}) がタイムアウトされました。")
+            await message.channel.send(f"{ポテトがファックされました。")
             # duration = datetime.timedelta(seconds=40, minutes=0, hours= 0, days=0)
             # await target_user.timeout(duration, reason="gay")
             
