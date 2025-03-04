bbb4b99f41de7b54156000db7b01f93a15866f82
isaz33 <subsy1233@gmail.com>
2025-03-04 15:39:12 +0900

    Update poteto.py

--- poteto.py
+++ poteto.py
@@ -10,6 +10,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command()
 async def timeout(ctx, member: discord.Member, minutes: int):
     try:
+        print("timeout")
         await member.timeout_for(minutes * 60)  # タイムアウト時間は秒単位
         await ctx.send(f"{member} has been timed out for {minutes} minutes.")
     except discord.Forbidden:
