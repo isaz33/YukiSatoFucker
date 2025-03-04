9e6793c8b1fcef73e271c977a0c350fb4174ca18
isaz33 <subsy1233@gmail.com>
2025-03-04 23:23:31 +0900

    Update poteto.py

--- poteto.py
+++ poteto.py
@@ -16,16 +16,6 @@ client = discord.Client(intents = intents)
 
 CHANNEL_ID = 727020336293609522
 
-# 任意のチャンネルで挨拶する非同期関数を定義
-async def greet():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('おはよう！')
-
-# bot起動時に実行されるイベントハンドラを定義
-@bot.event
-async def on_ready():
-    await greet() # 挨拶する非同期関数を実行
-
 
 # メッセージ受信時に動作する処理
 @bot.event
@@ -39,14 +29,15 @@ async def on_message(message):
         
 @bot.command()
 async def timeout(ctx, member: discord.Member, minutes: int):
-    try:
-        print("timeout")
-        await member.timeout_for(minutes * 10)  # タイムアウト時間は秒単位
-        await ctx.send(f"{member} has been timed out for {minutes} minutes.")
-    except discord.Forbidden:
-        await ctx.send("I don't have permission to timeout this user.")
-    except discord.HTTPException as e:
-        await ctx.send(f"An error occurred: {str(e)}")
+    await message.channel.send('タイムアウトを実行します。')
+    # try:
+    #     print("timeout")
+    #     await member.timeout_for(minutes * 10)  # タイムアウト時間は秒単位
+    #     await ctx.send(f"{member} has been timed out for {minutes} minutes.")
+    # except discord.Forbidden:
+    #     await ctx.send("I don't have permission to timeout this user.")
+    # except discord.HTTPException as e:
+    #     await ctx.send(f"An error occurred: {str(e)}")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
